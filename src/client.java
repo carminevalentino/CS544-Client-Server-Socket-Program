db2a874b1ca63f50ef571f467e31acf327b8cb1d
Carmine Valentino <cvalentino3@gmail.com>
2015-04-28 20:01:39 -0400

    Added Comments

--- src/client.java
+++ src/client.java
@@ -1,6 +1,7 @@
 // ================== File Abstract ====================
 // # 
 // # Name: Carmine Valentino
+// # File: client.java
 // # Program: TCP/UDP Client-Server Socket Program
 // # Date: April 19, 2015
 // # Course: CS 544 - Computer Networking
@@ -13,11 +14,15 @@ import java.net.*;
 
 public class client {
 	
+	// Static Variables Defined for Max Packet Size
+	// Note: Message is comprised of a 16Byte Payload
+	// And, a 2Byte Header
 	private static final int MAX_PAYLOAD_SIZE = 16;
 	private static final int MAX_HEADER_SIZE = 2;
 	private static final int MAX_BUFFER_SIZE = MAX_PAYLOAD_SIZE + MAX_HEADER_SIZE;
 
 	public static void main(String[] args) {
+		// -- Declare Objects/Variables --
 		String hostname;
 		int n_port;
 		int r_port = 0;
@@ -29,11 +34,14 @@ public class client {
 		DatagramPacket packetOutput = null;
 		DatagramPacket packetInput = null;
 		InetAddress _hostname = null;
-		
 		byte[] incomingPacket = null;
-		
+
+		// -------------------------------
+
+		// Check for Startup Parameters
 		if (args.length > 2)
 		{	
+			// Set Flags
 			hostname = args[0];
 			n_port = Integer.parseInt(args[1]);
 			filename = args[2];
@@ -41,6 +49,7 @@ public class client {
 			try
 			{
 				System.out.println("Attempting to connect to "+ hostname + ":" + n_port + " ...");
+				// Attempt To Establish Socket Connection at Hostname:Port Specified At Run-time
 				clientSocket = new Socket(hostname, n_port);
 				System.out.println("Connection Established ...");
 			}
@@ -51,7 +60,7 @@ public class client {
 				System.exit(1);
 			}
 			
-			// Sending 117 Ack Out
+			// Try Sending 117 Ack To Server
 			try
 			{
 				dataOutput = new DataOutputStream(clientSocket.getOutputStream());
@@ -62,6 +71,7 @@ public class client {
 				System.out.println("Unable to Transmit Ack to Server ...");
 				System.exit(1);
 			}
+			// Try Getting Random UDP Data Port From Server
 	        try
 	        {
 	        	dataInput = new DataInputStream(clientSocket.getInputStream());
@@ -73,6 +83,7 @@ public class client {
 	        {
 	        	System.out.println("Unable to Get Input From the Server ...");
 	        }
+	        // Attempt to Close TCP Sockets
 	        try
 	        {
 		        dataInput.close();
@@ -83,22 +94,25 @@ public class client {
 	        {
 	        	System.out.println("Unable to Close Sockets/Streams ...");
 	        }
+	        // Begin Message Sending Attempt
 	        try
 	        {
+	        	// Declare Local Variables/Objects
 	        	byte[] byteArray = new byte[(int) file.length()];
 	        	byte[] packetBuffer = new byte[MAX_BUFFER_SIZE];
 	        	byte[] headerBuffer = new byte[MAX_HEADER_SIZE];
 	        	byte[] payloadBuffer = new byte[MAX_PAYLOAD_SIZE];
-	        	
 	        	int byteArrayIndex = 0;
 	        	int numberOfPackets = 0;
 	        	int packetIndex = 0;
 	        	FileInputStream fileInput = new FileInputStream(file);
 	        	fileInput.read(byteArray);
 	        	int fileSize = (int)file.length();
-	        	// The Last Packet Contains less than Max Message Size
+	        	
+	        	// The Last Packet Contains less than Max Payload Size
 	        	if (fileSize % MAX_PAYLOAD_SIZE > 0)
 	        	{
+	        		// We Need to Send an Additional Packet
 	        		numberOfPackets = (fileSize/MAX_PAYLOAD_SIZE) + 1;
 	        	}
 	        	else
@@ -108,50 +122,59 @@ public class client {
 	        	System.out.println("Number of Packets to Be Sent " + numberOfPackets + "...");
 	        	System.out.println("Total Byte Size " + fileSize + "...");
 	        	
+	        	// Consider Each Byte in Our File
 	        	for(int currByte = 0; currByte < file.length(); ++currByte)
 	        	{
+	        		// Check our Byte Array Index Counter to Ensure
+	        		/// That we are generating a buffer within the
+	        		// Max Payload Size enforced (16 bytes)
 	        		if(byteArrayIndex < MAX_PAYLOAD_SIZE)
 	        		{
-	        			payloadBuffer[byteArrayIndex] = byteArray[currByte];
-	        			++byteArrayIndex;
-	        			// Last Byte of File Reached
+	        			payloadBuffer[byteArrayIndex] = byteArray[currByte]; // Add Current Byte to Current Payload Buffer
+	        			++byteArrayIndex; // Increment our Byte Index
+	        			
+	        			// *We determine that the last byte
+	        			// Is equivalent to the file size less
+	        			// The current byte in the file
+	        			// This will be our final byte and therefore
+	        			// We should prepare to severe connections*
 	        			if((fileSize - currByte) == 1)
 	        			{
 		        			headerBuffer = buildPacketHeader(packetIndex, -1); // -1 Indicates End of File
-		        			System.arraycopy(payloadBuffer, 0, packetBuffer, 0, MAX_PAYLOAD_SIZE);
-		        			System.arraycopy(headerBuffer, 0, packetBuffer, MAX_PAYLOAD_SIZE, MAX_HEADER_SIZE);
+		        			System.arraycopy(payloadBuffer, 0, packetBuffer, 0, MAX_PAYLOAD_SIZE); // Add our payload buffer to our outgoing buffer
+		        			System.arraycopy(headerBuffer, 0, packetBuffer, MAX_PAYLOAD_SIZE, MAX_HEADER_SIZE);// Add our header buffer to our outgoing buffer
 		        			try
 		        			{
-		        				_hostname = InetAddress.getByName(hostname);
-		        				packetOutput = new DatagramPacket(packetBuffer, packetBuffer.length, _hostname, r_port);
-		        				dataSocket.send(packetOutput);
+		        				_hostname = InetAddress.getByName(hostname); // Get InetAddres Type from String
+		        				packetOutput = new DatagramPacket(packetBuffer, packetBuffer.length, _hostname, r_port); // Create a new Datagram Object with our buffer
+		        				dataSocket.send(packetOutput); // Send Packet
 		        				dataSocket.close(); //Last Packet, so Lets Close Connection on Client end
 		        			}
 		        			catch (IOException error)
 		        			{
 		        				System.out.println("Unable to Transmit Packet ...");
 		        			}
-		        			String packetToString = new String(payloadBuffer);
+		        			String packetToString = new String(payloadBuffer); // Print Packet Output
 		        			System.out.println(packetToString.toUpperCase());
-		        			++packetIndex;
+		        			++packetIndex; // Increment our Packet Index For Good Measure
 	        			}
 	        		}
-	        		else
+	        		else // This is the Final Byte in Our Packet (But NOT the final Packet)
 	        		{
-	        			headerBuffer = buildPacketHeader(packetIndex, 0); // 0 Indicates More Packets to Come!
-	        			System.arraycopy(payloadBuffer, 0, packetBuffer, 0, MAX_PAYLOAD_SIZE);
-	        			System.arraycopy(headerBuffer, 0, packetBuffer, MAX_PAYLOAD_SIZE, MAX_HEADER_SIZE);
+	        			headerBuffer = buildPacketHeader(packetIndex, 0); // 0 Indicates More Packets to Come! (This is our "last packet" indicator)
+	        			System.arraycopy(payloadBuffer, 0, packetBuffer, 0, MAX_PAYLOAD_SIZE); // Add our payload buffer to our outgoing buffer
+	        			System.arraycopy(headerBuffer, 0, packetBuffer, MAX_PAYLOAD_SIZE, MAX_HEADER_SIZE); // Add our header buffer to our outgoing buffer
 	        			try
 	        			{
-	        				_hostname = InetAddress.getByName(hostname);
-	        				packetOutput = new DatagramPacket(packetBuffer, packetBuffer.length, _hostname, r_port);
-	        				dataSocket = new DatagramSocket();
-	        				dataSocket.send(packetOutput);
-	        				try
+	        				_hostname = InetAddress.getByName(hostname);// Get InetAddres Type from String
+	        				packetOutput = new DatagramPacket(packetBuffer, packetBuffer.length, _hostname, r_port);// Create a new Datagram Object with our buffer
+	        				dataSocket = new DatagramSocket(); // Create Our Socket
+	        				dataSocket.send(packetOutput);// Send Packet
+	        				try // Attempt to get ack from server (For packet receipt)
 	        				{
-		        				incomingPacket = new byte[MAX_BUFFER_SIZE];
-		        				packetInput = new DatagramPacket(incomingPacket, incomingPacket.length, _hostname, r_port);
-		        				dataSocket.receive(packetInput);
+		        				incomingPacket = new byte[MAX_BUFFER_SIZE]; // Create incoming packet buffer
+		        				packetInput = new DatagramPacket(incomingPacket, incomingPacket.length, _hostname, r_port); // Create Datagram Object with our buffer
+		        				dataSocket.receive(packetInput); // Get Ack Back From Server, Acknowledging receipt of our packet
 		        			}
 	        				catch(IOException error)
 	        				{
@@ -164,13 +187,13 @@ public class client {
 	        				System.out.println("Unable to Transmit Packet ...");
 	        			}
 	        			String packetToString = new String(payloadBuffer);
-	        			System.out.println(packetToString.toUpperCase()); //TODO: Send Packet
-	        			++packetIndex;
-	        			byteArrayIndex = 0;
-	        			payloadBuffer = null;
-	        			payloadBuffer = new byte[MAX_PAYLOAD_SIZE];
-	        			payloadBuffer[byteArrayIndex] = byteArray[currByte];
-	        			++byteArrayIndex;
+	        			System.out.println(packetToString.toUpperCase()); // Print our Packet
+	        			++packetIndex; // Increment Packet Index To determine which packet we are to transmit next
+	        			byteArrayIndex = 0; // Clear Byte Index [0-15]
+	        			payloadBuffer = null; // Set Overall buffer to Null
+	        			payloadBuffer = new byte[MAX_PAYLOAD_SIZE]; // Reallocate our Buffer
+	        			payloadBuffer[byteArrayIndex] = byteArray[currByte]; // * Set First Byte for our Next Packet
+	        			++byteArrayIndex; // Increment Byte Index
 	        		}
 	        	}
 	        }
@@ -179,17 +202,18 @@ public class client {
 	        	System.out.print(error);
 	        }
 		}
-		else
+		else // Catch Erroneous Input Parameters
 		{
 			System.out.println("You must specify the following: hostname, port, file...");
 			System.exit(1);
 		}
 	}
+	// Constructs our Header Buffer for Each Packet
 	private static byte[] buildPacketHeader(int _sequence, int _eof)
 	{
 		byte[] packetHeader = new byte[MAX_HEADER_SIZE];
-		packetHeader[0] = (byte) _sequence;
-		packetHeader[1] = (byte) _eof;
-		return packetHeader;
+		packetHeader[0] = (byte) _sequence; // Packet #
+		packetHeader[1] = (byte) _eof; // End of File or Packet Indicator
+		return packetHeader; 
 	}
 }
--- src/server.java
+++ src/server.java
@@ -16,16 +16,20 @@ import java.util.Random;
 
 public class server {
 	
+	// Static Variables Defined for Max Packet Size
+	// Note: Message is comprised of a 16Byte Payload
+	// And, a 2Byte Header
 	private static final int MAX_PAYLOAD_SIZE = 16;
 	private static final int MAX_HEADER_SIZE = 2;
 	private static final int FINAL_PACKET = -1;
 	private static final int MAX_BUFFER_SIZE = MAX_PAYLOAD_SIZE + MAX_HEADER_SIZE;
 	
 	public static void main(String[] args) throws IOException {
-		int n_port;
+		// Check for Startup Parameters
 		if (args.length > 0)
 		{
-			n_port = Integer.parseInt(args[0]);
+			// -- Declare Objects/Variables --
+			int n_port = Integer.parseInt(args[0]);
 			int _ack = 0;
 			DataInputStream dataInput = null;
 			DataOutputStream dataOutput = null;
@@ -38,6 +42,9 @@ public class server {
 			byte[] incomingPacketBuffer = new byte[MAX_BUFFER_SIZE];
 			byte[] outgoingPacketBuffer  = new byte[MAX_BUFFER_SIZE];
 			DatagramPacket ackPacket = null;
+			// -------------------------------
+			
+			// Atempt to Open TCP socket at Given Port
 			try
 			{
 				serverSocket = new ServerSocket(n_port);
@@ -49,7 +56,7 @@ public class server {
 				System.exit(1);
 			}
 			System.out.println("Server Listening for Connections on Port " + n_port + " ...");
-			try
+			try //Listen for Comms on Socket
 			{
 				clientSocket = serverSocket.accept();
 			}
@@ -59,7 +66,7 @@ public class server {
 				System.out.println("Unable to Accept Connection from Client ...");
 				System.exit(1);
 			}
-			try
+			try // Attempt to Receive (Int)Ack from Client
 			{
 				dataInput = new DataInputStream(clientSocket.getInputStream());
 				_ack = dataInput.readInt();
@@ -70,57 +77,58 @@ public class server {
 				System.out.println("Unable to Accept Ack from Client ...");
 				System.exit(1);
 			}
-			if (117 == _ack)
+			if (117 == _ack)// Authenticate Comms by analyzing Ack for 117 sent from Client
 			{
 				System.out.println("Received an Ack of: " + _ack + " From the Client ...");
-				dataOutput = new DataOutputStream(clientSocket.getOutputStream());
-				_r_port = randomPort();
+				dataOutput = new DataOutputStream(clientSocket.getOutputStream()); //Open a Data Stream with Client
+				_r_port = randomPort(); // Generate Random UDP Port
 				System.out.println("Generated Random UDP Port " + _r_port + ". Sending to Client ...");
-				dataOutput.writeInt(_r_port);
-				dataSocket = new DatagramSocket(_r_port);
-				File textFile = new File("receive.txt"); // Create New Text File
+				dataOutput.writeInt(_r_port); // Send Random UDP port to Client
+				closeTcpConnection(serverSocket); // Terminate TCP Socket
+				dataSocket = new DatagramSocket(_r_port); // Create a Socket (UDP) at Random Port
+				File textFile = new File("receive.txt"); // Create New Text File To Write our Input to
 			}
 			else
 			{
 				System.out.println("Invalid Ack Received ...");
-				dataInput.close();
-				closeTcpConnection(serverSocket);
+				dataInput.close(); // Close Data Stream
+				closeTcpConnection(serverSocket); // Terminate TCP Socket
 			}
 			System.out.println("Listening for Packets ...");
 			try
 			{
-				while (true)
+				while (true) // Listen to Packets indefinitely (Until Last Packet is Realized, then Exit)
 				{
-					DatagramPacket packet = new DatagramPacket(incomingPacketBuffer, incomingPacketBuffer.length);
-					dataSocket.receive(packet);
-					byte[] _header = new byte[MAX_HEADER_SIZE];
-					byte[] _payload = new byte[MAX_PAYLOAD_SIZE];
+					DatagramPacket packet = new DatagramPacket(incomingPacketBuffer, incomingPacketBuffer.length); // New Datagram Packet Created For Input Buffer
+					dataSocket.receive(packet); // Get Packet from Client
+					byte[] _header = new byte[MAX_HEADER_SIZE]; // Allocate Header Buffer
+					byte[] _payload = new byte[MAX_PAYLOAD_SIZE]; // Allocate Payload Buffer
 					_header = Arrays.copyOfRange(incomingPacketBuffer, 16, 18); // Header in Last 2 Bytes of Packet
 					_payload = Arrays.copyOfRange(incomingPacketBuffer, 0, 16); // Payload in First 16 Bytes of Packet
 					// -- HEADER -- //
 					int _sequence = (int)_header[0]; // Packet Sequence (Added to Ensure No Packet Loss Occured)
 					int _eof = (int)_header[1]; // Final Packet Transmission Byte (0 = no, -1 = yes)
 					// -- PAYLOAD -- //
-					String payloadString = new String(_payload);
-					outgoingPacketBuffer = new String(incomingPacketBuffer).toUpperCase().getBytes();
-					DatagramPacket outgoingPacket = new DatagramPacket(outgoingPacketBuffer, outgoingPacketBuffer.length, packet.getAddress(), packet.getPort());
-					dataSocket.send(outgoingPacket);
+					String payloadString = new String(_payload); 
+					outgoingPacketBuffer = new String(incomingPacketBuffer).toUpperCase().getBytes(); // Convert Buffer to Upper Case For purpose of Ack'ing
+					DatagramPacket outgoingPacket = new DatagramPacket(outgoingPacketBuffer, outgoingPacketBuffer.length, packet.getAddress(), packet.getPort()); //Create Outgoing Datagram to Send Ack
+					dataSocket.send(outgoingPacket);// Send Packet Ack
 					if (0 == _sequence) //First Packet, Clear Our Output File If Necessary
 					{
 						FileOutputStream initialFileOutput = new FileOutputStream("receive.txt");
-						initialFileOutput.write((new String()).getBytes());
+						initialFileOutput.write((new String()).getBytes()); // Write Empty Buffer to Receive.txt to Clear File (Just in Case ..)
 					}
 					
-					FileOutputStream fileOutput = new FileOutputStream("receive.txt",true);
+					FileOutputStream fileOutput = new FileOutputStream("receive.txt",true); //Create New File Output Stream With Append Flag
 					fileOutput.write(_payload); // Write Payload To File "receive.txt"
 					if (FINAL_PACKET == _eof) // Final Packet Byte Received
 					{
 						System.out.println("Final Packet Received ...");
 						System.out.println("Writing File ...");
-						fileOutput.close();
+						fileOutput.close(); // Close File
 						System.out.println("Closing Socket ...");
-						dataSocket.close();
-						System.exit(0);
+						dataSocket.close(); // Terminate Socket Connection
+						System.exit(0); // It is Time to Exit!
 					}
 				}
 			}
@@ -129,16 +137,19 @@ public class server {
 				System.out.println("Unable To Receive Packet ...");
 			}
 		}
-		else
+		else // Catch Flag Errors
 		{
 			System.out.println("You must specify a Port...");
 			System.exit(1);
 		}
 	}
+	// TCP Socket Close
 	private static void closeTcpConnection(ServerSocket _serverSocket) throws IOException
 	{
 		_serverSocket.close();
 	}
+	
+	// Check Availability of Port
 	private static boolean isUdpAvailable(int _r_port)
 	{
 		try
@@ -155,15 +166,17 @@ public class server {
 		}
 		return true;
 	}
+	
+	// Generate Random UDP Por
 	private static int randomPort()
 	{
 		// Create Random Object
 		Random random = new Random();
 		//Generate a Random Int between our Port Ranges (1024 -> 65535)
 		int r_port = random.nextInt((65535 - 1024) + 1) + 1024;
-		if(!isUdpAvailable(r_port))
+		if(!isUdpAvailable(r_port)) // Check if Port is Available Before Creating
 		{
-			randomPort();
+			randomPort(); // Call Random Port Again To Randomly Generate Port Again
 		}
 		return r_port;
 	}
